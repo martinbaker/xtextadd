2864162f96534a379b27d1abbdc2a03c873f43d3
martinbaker <git87465@martinb.com>
2015-01-12 09:35:29 +0000

    correct phantom token to increment indexes

--- nodemodeltest/com.euclideanspace.phantom1/src/com/euclideanspace/phantom1/PhantomToken.java
+++ nodemodeltest/com.euclideanspace.phantom1/src/com/euclideanspace/phantom1/PhantomToken.java
@@ -90,8 +90,8 @@ public class PhantomToken extends CommonToken {
 		super(previousToken.getInputStream(),
 			type,
 			previousToken.getChannel(),
-			previousToken.getStopIndex(), // start index set to STOP index of previous
-			previousToken.getStopIndex()-1); // stop index also set to stop index of previous
+			previousToken.getStopIndex()+1, // start index set to STOP index of previous
+			previousToken.getStopIndex()); // stop index also set to stop index of previous
 		/* We must set the text to an empty string rather than leaving it null. This is
 		 * because, if text value is left at null, then there would be an attempt to fetch
 		 * text from character stream which is not what we want.*/
